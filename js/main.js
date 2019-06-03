cf9e8cf77eda27a20d7ae69679c49def7abc0024
Tad Nikolaeva <tad.nikolaeva@gmail.com>
2019-06-03 16:33:58 +0500

    доработка внешнего вида

--- js/main.js
+++ js/main.js
@@ -33,6 +33,14 @@
 		}, 600);
 	});
 
+	$(".scroll-to-contacts").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
+
 	$('#back-to-top').on('click', function(){
 		$('body,html').animate({
 			scrollTop: 0
